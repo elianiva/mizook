a2dd6ad8a4d473cf56bdd14c1c03f25574acf325
elianiva <git@elianiva.my.id>
2026-04-17 18:25:47 +0700

    feat: scaffold telegram bot

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-export default {
-  async fetch(request: Request): Promise<Response> {
-    const url = new URL(request.url);
+import { Bot, webhookCallback } from "grammy";
 
-    if (url.pathname === "/") {
-      return new Response("Hello, 世界!");
+export interface Env {
+  BOT_TOKEN: string;
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url);
+    const bot = new Bot(env.BOT_TOKEN);
+
+    bot.command("start", async (ctx) => {
+      await ctx.reply("Started!");
+    });
+
+    bot.on("message:text", async (ctx) => {
+      await ctx.reply("It's working!");
+    });
+
+    if (url.pathname === "/telegram") {
+      return webhookCallback(bot, "cloudflare-mod")(request);
     }
 
-    return new Response("not found", { status: 404 });
+    return new Response("Nothing to see here...", { status: 404 });
   },
-} satisfies ExportedHandler;
+} satisfies ExportedHandler<Env>;
